09811b6db565dfafa7f6315b40afed01c947f9d2
peedy2495 <mail.petermark@web.de>
2021-11-08 07:43:01 +0100

    bugfix

--- assets/glance/glance-api.sh
+++ assets/glance/glance-api.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+apt -y install glance 
+
 # wait for nova api
 WaitForHost $CTRL_HOST_IP 5000 tcp 'keystone@'
 
 mkfs.ext4 $IMGDEV
 mount -t ext4 $IMGDEV /var/lib/glance/images
 echo "/dev/$IMGDEV    /var/lib/glance/images          ext4      rw,relatime,nodev,nosuid,noexec              0       1" >>/etc/fstab
-
-apt -y install glance 
 
 for var in CTRL_HOST_IP ADMPWD SERVPWD; do
     ReplVar $var $ASSETS/glance/glance-api.conf
